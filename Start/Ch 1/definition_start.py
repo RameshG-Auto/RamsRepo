27ec7273d794d14beb50c0d5fb464391575cc0f6
astroJarHead <73755825+astroJarHead@users.noreply.github.com>
2024-12-14 00:15:14 +0000

    Changes to Chapter 1 follwoing lesson.

--- Start/Ch 1/definition_start.py
+++ Start/Ch 1/definition_start.py
@@ -3,9 +3,16 @@
 
 
 # TODO: create a basic class
-
+class Book:
+    def __init__(self, title):
+        self.title = title
 
 # TODO: create instances of the class
+book1 = Book("Seven Pillars of Wisdom")
+book2 = Book("Lord of the Rings")
 
-
-# TODO: print the class and property
+# TODO: print the class object and property
+print(book1)
+print(book1.title)
+print(book2)
+print(book2.title)
