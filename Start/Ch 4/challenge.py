e16e2721b5e252171fc5d4c28bdf2d84149aa828
DonArchila <kevin.archila@gmail.com>
2024-12-25 14:39:22 -0700

    Implement Asset, Stock, and Bond classes with string representation and comparison methods

--- Start/Ch 4/challenge.py
+++ Start/Ch 4/challenge.py
@@ -5,17 +5,40 @@
 # The subclasses are required to override the magic method
 # that makes them sortable
 
+#%%
+
+from dataclasses import dataclass
+
+@dataclass
 class Asset():
-    pass
+    price: float
+
+    def __str__(self):
+        pass
+    def __lt__(self, other):
+        pass
     
-
+@dataclass
 class Stock(Asset):
-    pass
+    ticker: str
+    company: str
 
+    def __str__(self):
+        return f"{self.ticker}: {self.company} -- ${self.price}"
 
+    def __lt__(self, other):
+        return self.price < other.price
+@dataclass
 class Bond(Asset):
-    pass
+    description: str
+    duration: int
+    yieldamt: float
 
+    def __str__(self):
+        return f"{self.description}: {self.duration}yr : ${self.price} : {self.yieldamt}%"
+    
+    def __lt__(self, other):
+        return self.yieldamt < other.yieldamt
 # ~~~~~~~~~ TEST CODE ~~~~~~~~~
 stocks = [
     Stock("MSFT", 342.0, "Microsoft Corp"),
@@ -44,3 +67,5 @@ for stock in stocks:
 print("-----------")
 for bond in bonds:
    print(bond)
+
+# %%
